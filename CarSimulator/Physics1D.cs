eb61098adc283c4ba198cf2c86ff5387de9df2b8
RyanmLin <64663423+RyanmLin@users.noreply.github.com>
2021-09-13 14:15:09 -0700

    Finished Part 1
    Added in Math Function for Physic1D.cs
    Added in Functions in Program.cs

--- CarSimulator/Physics1D.cs
+++ CarSimulator/Physics1D.cs
@@ -1,10 +1,40 @@
 ﻿using System;
+
 namespace CarSimulator
 {
 
     public class Physics1D
     {
         // Implement the methods
-
+        public static double compute_position(double x0, double v, double dt)
+        { 
+            // to do
+            var position = x0 + (v * dt); // Math to solve for postion
+            return position;
+        }
+        public static double compute_velocity(double v0, double a, double dt)
+        { 
+            // to do
+            var velocity = v0 + (a * dt); // Math to solve for velocity
+            return velocity;
+        }
+        public static double compute_velocity(double x0, double t0, double x1, double t1)
+        { 
+            // to do
+            var velocity = (x1 - x0)/(t1 - t0); // Math to solve for velocity
+            return velocity;
+        }
+        public static double compute_acceleration(double v0, double t0, double v1, double t1)
+        { 
+            // to do
+            var acceleration  = (v1 - v0)/(t1 - t0); // Math to solve for acceleration
+            return acceleration;
+        }
+        public static double compute_acceleration(double f, double m)
+        { 
+            // to do
+            var acceleration = f / m; // Math to solve for acceleration
+            return acceleration;
+        }
     }
 }
